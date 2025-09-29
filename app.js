89f01f6715ffbc1b841131879c95e864222ef381
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-29 09:06:07 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Funktionalität für die AR-Funktionalität wird hier implementiert.
+    console.log('AR Funktionalität wird initiiert.');
+});
