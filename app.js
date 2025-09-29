d848c26286c19afa77d8c76203185bdaf61661e5
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-29 09:07:02 -0500

    Updated log message for clarity on AR feature initialization.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Functionality for the AR feature will be implemented here.
-    console.log('AR functionality is being initiated.');
+    console.log('Initializing Augmented Reality functionality.');
 });
